fe24d1a0c13ab74dde4c8778ab63c3007557da43
Alfred <mylearning.jiayong@gmail.com>
2022-07-07 14:37:07 +0800

    [SCHEDULER][PYTHON] Add crawlers schedular

---
+++ crawler/scheduler.py
@@ -0,0 +1,26 @@
+import schedule
+import subprocess
+import time
+
+def do_taipei_youbike_crawling():
+    subprocess.run('python3 taipei_youbike_crawler.py', shell=True, cwd='crawlers')
+
+def do_taichung_youbike_crawling():
+    subprocess.run('python3 taichung_youbike_crawler.py', shell=True, cwd='crawlers')
+
+def do_precipitation_crawling():
+    subprocess.run('python3 precipitation_crawler.py', shell=True, cwd='crawlers')
+
+def do_wheather_crawling():
+    subprocess.run('python3 weather_crawler.py', shell=True, cwd='crawlers')
+
+if __name__ == '__main__':
+
+    schedule.every(1).minutes.do(do_taipei_youbike_crawling)
+    schedule.every(1).minutes.do(do_taichung_youbike_crawling)
+    schedule.every(10).minutes.do(do_precipitation_crawling)
+    schedule.every(10).minutes.do(do_wheather_crawling)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
