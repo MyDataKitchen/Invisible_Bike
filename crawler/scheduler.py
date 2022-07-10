9727c6eb9dca4a0053c7d2b4ffd72d809f365008
Alfred <mylearning.jiayong@gmail.com>
2022-07-10 11:01:25 +0800

    [SCHEDULAR][PYTHON] Add threading for crawler functions

--- crawler/scheduler.py
+++ crawler/scheduler.py
@@ -1,25 +1,33 @@
 import schedule
 import subprocess
 import time
+import threading
 
-def do_taipei_youbike_crawling():
+def taipei_youbike():
     subprocess.run('python3 taipei_youbike_crawler.py', shell=True, cwd='crawlers')
 
-def do_taichung_youbike_crawling():
+def taichung_youbike():
     subprocess.run('python3 taichung_youbike_crawler.py', shell=True, cwd='crawlers')
 
-def do_precipitation_crawling():
-    subprocess.run('python3 precipitation_crawler.py', shell=True, cwd='crawlers')
 
-def do_wheather_crawling():
+def do_youbike_crawling():
+    print("s taipei")
+    thread_1 = threading.Thread(target=taipei_youbike)
+    print("s taichung")
+    thread_2 = threading.Thread(target=taichung_youbike)
+    thread_1.start()
+    thread_2.start()
+    thread_1.join()
+    thread_2.join()
+
+def do_precipitation_and_wheather_crawling():
+    subprocess.run('python3 precipitation_crawler.py', shell=True, cwd='crawlers')
     subprocess.run('python3 weather_crawler.py', shell=True, cwd='crawlers')
 
 if __name__ == '__main__':
 
-    schedule.every(1).minutes.do(do_taipei_youbike_crawling)
-    schedule.every(1).minutes.do(do_taichung_youbike_crawling)
-    schedule.every(10).minutes.do(do_precipitation_crawling)
-    schedule.every(10).minutes.do(do_wheather_crawling)
+    schedule.every(1).minutes.do(do_youbike_crawling)
+    schedule.every(10).minutes.do(do_precipitation_and_wheather_crawling)
 
     while True:
         schedule.run_pending()
