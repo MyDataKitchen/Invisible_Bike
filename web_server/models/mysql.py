ac0fe6f7c86f6d309e3acc6a16d36d948b3ff885
Alfred <mylearning.jiayong@gmail.com>
2022-07-17 06:46:38 +0800

    [MYSQL][PYTHON] Add get date function

--- web_server/models/mysql.py
+++ web_server/models/mysql.py
@@ -19,3 +19,10 @@ def get_all_stations(city):
     df = pd.read_sql_query(query, engine)
     stations_id = df['stationId'].tolist()
     return df, stations_id
+
+def get_date():
+    query = f"SELECT datetime FROM mysql_data_processed ORDER BY id DESC LIMIT 1"
+    result = engine.execute(query)
+    date = result.fetchall()
+    result.close()
+    return date[0][0].date()
